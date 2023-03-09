4ce3e255aa43e8c65f530d4ea396a05dda0dffd2
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-03-09 15:22:02 -0500

    c12 affichage et animation de la boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,19 @@
 (function () {
     // fonction IFEE
     console.log('debut du carrousel');
+    let bouton__ouvrir = document.querySelector('.bouton__ouvrir')
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmBouton__x = document.querySelector('.bouton__x')
+    console.log(bouton__ouvrir.tagName)
+
+    bouton__ouvrir.addEventListener('mousedown', function () {
+        console.log('boite modale')
+        elmCarrousel.classList.add('carrousel--ouvrir')
+    })
+
+    elmBouton__x.addEventListener('mousedown', function () {
+        console.log('boite modale')
+        elmCarrousel.classList.remove('carrousel--ouvrir')
+    })
+
 })()
