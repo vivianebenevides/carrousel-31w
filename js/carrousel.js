b4164878ad7c3a65cf1e7722f2b1ef695ddbdd56
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-03-09 16:57:39 -0500

    c12 carrousel fonctionnel

--- js/carrousel.js
+++ js/carrousel.js
@@ -6,7 +6,8 @@
     let elmBouton__x = document.querySelector('.bouton__x')
     let elmGalerie = document.querySelector('.galerie')
     let elmGalerie__img = elmGalerie.querySelectorAll('img')
-    let elmCarrousel__figure = document.querySelector('.carrousel__figure')
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure') // conteneur des images
+    let elmCarrousel__form = document.querySelector('.carrousel__form') //conteneur des radio bouton
     console.log(elmGalerie__img.length)
 
 
@@ -27,13 +28,42 @@
     function ajouter_carrousel() {
         for (const elmImg of elmGalerie__img) {
             ajouter_img(elmImg) //ajoute l'image dans le carrousel 
+            ajouter_radio() //ajoute les radio bouton dans carrousel__form
         }
+        elmCarrousel__figure.children[0].classList.add('carrousel__img--activer')
     }
 
     function ajouter_img(elmImg) {
         let elmCarrousel__img = document.createElement('img')
         elmCarrousel__img.setAttribute('src', elmImg.getAttribute('src'))
+        elmCarrousel__img.classList.add('carrousel__img')
+        elmCarrousel__img.dataset.index = index
         elmCarrousel__figure.appendChild(elmCarrousel__img)
     }
+
+    let index = 0;
+    let index__precedent = -1;
+
+    function ajouter_radio() {
+        let elmCarrousel__radio = document.createElement('input')
+        elmCarrousel__radio.setAttribute('type', 'radio')
+        elmCarrousel__radio.setAttribute('name', 'radCarrousel')
+        elmCarrousel__radio.dataset.index = index
+        index++
+        elmCarrousel__form.appendChild(elmCarrousel__radio)
+        elmCarrousel__radio.addEventListener('mousedown', function () {
+            activer__image(this.dataset.index)
+        })
+    }
+
+    function activer__image(index) {
+        //falta fazer abetura do carrousel clicando sobre uma img, e falta flecha do carrolel para poder navergar. falt colocar bouton radio e botao y em posicao absoluta
+        if (index__precedent != -1) {
+            elmCarrousel__figure.children[index__precedent].classList.remove('carrousel__img--activer')
+        }
+        elmCarrousel__figure.children[index].classList.add('carrousel__img--activer')
+        index__precedent = index;
+    }
+
 
 })()
