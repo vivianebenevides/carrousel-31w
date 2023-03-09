e4fa19f36030fa7179e7b0aebd9ff1a38e820aa9
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-03-09 14:51:31 -0500

    c12 carrousel.php enqueue et generation du code HTML

--- carrousel.php
+++ carrousel.php
@@ -12,6 +12,12 @@ Plugin URI: https://github.com/vivianebenevides/carrousel-31w
 Description: Permet d'afficher dans une boîte dans les images d'une galerie avec un systheme de navigation
 */
 
+
+function carrousel_enqueue(){
+
+$version_css = filemtime(plugin_dir_path(__FILE__ ) . "style.css");
+$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
 wp_enqueue_style(   'em_plugin_carrousel_css',
 plugin_dir_url(__FILE__) . "style.css",
 array(),
@@ -22,3 +28,18 @@ plugin_dir_url(__FILE__) ."js/carrousel.js",
 array(),
 $version_js,
 true);
+}
+
+add_action('wp_enqueue_scripts', 'carrousel_enqueue');
+
+function creation_carrousel() 
+{
+return '<button class="bouton__ouvrir">Ouvrir</button>
+    <div class="carrousel">
+    <button class="bouton__x">X</button>
+    <figure class="carrousel__figure"></figure>
+    <form class="carrousel__form"></form>
+    </div>';
+}
+
+add_shortcode('carrousel', 'creation_carrousel');
